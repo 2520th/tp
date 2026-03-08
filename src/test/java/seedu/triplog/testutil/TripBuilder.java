82f93098aaf3a4c864350978ebb518206cf93b27
poise3 <105375844+poise3@users.noreply.github.com>
2026-03-08 21:21:34 +0800

    Update TripBuilder.java

--- src/test/java/seedu/triplog/testutil/TripBuilder.java
+++ src/test/java/seedu/triplog/testutil/TripBuilder.java
@@ -3,11 +3,7 @@ package seedu.triplog.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.triplog.model.person.Address;
-import seedu.triplog.model.person.Email;
-import seedu.triplog.model.person.Name;
-import seedu.triplog.model.person.Phone;
-import seedu.triplog.model.person.Trip;
+import seedu.triplog.model.person.*;
 import seedu.triplog.model.tag.Tag;
 import seedu.triplog.model.util.SampleDataUtil;
 
@@ -20,16 +16,16 @@ public class TripBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_START_DATE = "2026-01-01";
-    public static final String DEFAULT_END_DATE = "2026-01-10";
+    public static final String DEFAULT_START_DATE = "2026-02-02";
+    public static final String DEFAULT_END_DATE = "2026-02-03";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
-    private String startDate;
-    private String endDate;
     private Set<Tag> tags;
+    private TripDate startDate;
+    private TripDate endDate;
 
     /**
      * Creates a {@code TripBuilder} with the default details.
@@ -39,9 +35,9 @@ public class TripBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
-        startDate = DEFAULT_START_DATE;
-        endDate = DEFAULT_END_DATE;
         tags = new HashSet<>();
+        startDate = new TripDate(DEFAULT_START_DATE);
+        endDate = new TripDate(DEFAULT_END_DATE);
     }
 
     /**
@@ -52,9 +48,9 @@ public class TripBuilder {
         phone = tripToCopy.getPhone();
         email = tripToCopy.getEmail();
         address = tripToCopy.getAddress();
-        startDate = tripToCopy.getStartDate().toString();
-        endDate = tripToCopy.getEndDate().toString();
         tags = new HashSet<>(tripToCopy.getTags());
+        startDate = tripToCopy.getStartDate();
+        endDate = tripToCopy.getEndDate();
     }
 
     /**
@@ -97,23 +93,8 @@ public class TripBuilder {
         return this;
     }
 
-    /**
-     * Sets the {@code startDate} of the {@code Trip} being built.
-     */
-    public TripBuilder withStartDate(String startDate) {
-        this.startDate = startDate;
-        return this;
-    }
-
-    /**
-     * Sets the {@code endDate} of the {@code Trip} being built.
-     */
-    public TripBuilder withEndDate(String endDate) {
-        this.endDate = endDate;
-        return this;
-    }
-
     public Trip build() {
-        return new Trip(name, phone, email, address, tags);
+        return new Trip(name, phone, email, address, tags, startDate, endDate);
     }
+
 }
