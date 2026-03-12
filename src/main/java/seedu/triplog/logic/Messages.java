c1bae7e60a3c5a9c8bc3ea2026c367d369bc821e
lyx-888 <yixuanleong441@gmail.com>
2026-03-12 20:32:14 +0800

    Fix invalid command placeholder issue

--- src/main/java/seedu/triplog/logic/Messages.java
+++ src/main/java/seedu/triplog/logic/Messages.java
@@ -13,7 +13,7 @@ import seedu.triplog.model.trip.Trip;
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
-    //public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_DELETE_INVALID_COMMAND_FORMAT =
             "Error: Invalid command format. Usage: delete <index>";
     public static final String MESSAGE_INVALID_TRIP_DISPLAYED_INDEX = "Error: Trip index out of range.";
